6c65b72375bbce0cd42a0ef5414d21dc86fc99d6
sromankov <Sergey.Romankov@dataart.com>
2019-10-09 23:26:01 +0300

    initial commit

---
+++ build/app.js
@@ -0,0 +1,6 @@
+console.log("It works!");
+console.log("It works!");
+console.log("It works!");
+console.log("It works!");
+console.log("It works!");
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+console.log("It works!");
+console.log("It works!");
+console.log("It works!");
+console.log("It works!");
+console.log("It works!");
