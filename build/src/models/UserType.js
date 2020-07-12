ff9f56b2da8afe3d205b253a938b4cc193b7c87b
sromankov <Sergey.Romankov@dataart.com>
2020-07-12 19:41:53 +0300

    remoove

--- build/src/models/UserType.js
+++
@@ -1,5 +0,0 @@
-var UserType;
-(function (UserType) {
-    UserType["Vendor"] = "vendor";
-})(UserType || (UserType = {}));
-//# sourceMappingURL=UserType.js.map
--- build/src/pageobjects/BaseFragment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseFragment = void 0;
-const protractor_1 = require("protractor");
-class BaseFragment {
-}
-exports.BaseFragment = BaseFragment;
-BaseFragment.EC = protractor_1.protractor.ExpectedConditions;
-//# sourceMappingURL=BaseFragment.js.map
